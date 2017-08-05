a2b30b2297bf3c87024bdafc6d2a1365f1cdb26d
Ali Mousa <amousa11@gmail.com>
2017-08-05 09:36:11 -0700

    Greeter

--- setup-greeter/contracts/Greeter.sol
+++ setup-greeter/contracts/Greeter.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.4.11;
 contract greeter {
   /* Add one variable to hold our greeting (string)*/
-  string greeting;
+
 
   function greeter(string _greeting) public {
     /* Write one line of code for the contract to set our greeting*/
-    greeting = _greeting;
+    
   }
 
   function greet() constant returns (string)  {
     /* Write one line of code to allow the contract to return our greeting */
-    return greeting;
+    
   }
 }
